efdf467ef3b07ce530de30701f7a78d5f12a88d8
Huy Nguyen <nguyenhuy182515@gmail.com>
2020-08-21 14:47:35 +0700

    config secret key

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_admin import Admin
 from flask_login import LoginManager
 
 app = Flask(__name__)
-app.secret_key = "Q\abc\xyz\def!V"
+app.secret_key = "\xf4S\xb5\x1b4X\x97\x85\xdc\xc8\x91[6\xd6w\xd0"
 app.config["SQLALCHEMY_DATABASE_URI"]="mysql+pymysql://root:123456@localhost/bookstoredb?charset=utf8mb4"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 
@@ -12,4 +12,4 @@ db = SQLAlchemy(app=app)
 
 admin = Admin(app=app,name="QUAN LY NHA SACH", template_mode="bootstrap3")
 
-login = LoginManager(app=app)
+#login = LoginManager(app=app)
