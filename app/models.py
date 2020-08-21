cc169d106deeb29a58ae94318812b96e662deb13
Huy Nguyen <nguyenhuy182515@gmail.com>
2020-08-21 14:52:56 +0700

    create category manager for admin

--- app/models.py
+++ app/models.py
@@ -1,3 +1,22 @@
 from sqlalchemy import Column, Integer, String, Float, ForeignKey
 from sqlalchemy.orm import relationship
-from app import db
+from flask_admin.contrib.sqla import ModelView
+from app import db, admin
+
+
+class Category(db.Model):
+    __tablename__ = "category"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(50), nullable=False)
+    #books = relationship("Book",backref='category',lazy=True)
+
+    def __str__(self):
+        return self.name
+
+
+admin.add_view(ModelView(Category, db.session))
+
+
+if __name__ == "__main__":
+    db.create_all()
