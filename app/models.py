64b39cc33b8e7bec3bc7603cf064a3ae8e73305b
Huy Nguyen <nguyenhuy182515@gmail.com>
2020-08-22 13:34:32 +0700

    create book management

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Float, ForeignKey, Date
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
 from sqlalchemy.orm import relationship
 from flask_admin.contrib.sqla import ModelView
 from app import db, admin
@@ -9,7 +9,7 @@ class Category(db.Model):
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(50), nullable=False)
-    #books = relationship("Book",backref='category',lazy=True)
+    books = relationship("Book", backref='category', lazy=True)
 
     def __str__(self):
         return self.name
@@ -22,14 +22,30 @@ class Author(db.Model):
     firstname = Column(String(10), nullable=False)
     lastname = Column(String(50), nullable=False)
     dateofbirth = Column(String(10))
+    books = relationship("Book", backref='author', lazy=True)
 
     def __str__(self):
         return self.lastname + " " + self.firstname
 
 
+class Book(db.Model):
+    __tablename__ = "book"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    title = Column(String(100), nullable=False)
+    description = Column(String(255), nullable=True)
+    price = Column(Float, default=0)
+    image = Column(String(255), nullable=True)
+    category_id = Column(Integer, ForeignKey(Category.id), nullable=False)
+    author_id = Column(Integer, ForeignKey(Author.id), nullable=False)
+
+    def __str__(self):
+        return self.title
+
+
 admin.add_view(ModelView(Category, db.session))
 admin.add_view(ModelView(Author, db.session))
-
+admin.add_view(ModelView(Book, db.session))
 
 if __name__ == "__main__":
     db.create_all()
