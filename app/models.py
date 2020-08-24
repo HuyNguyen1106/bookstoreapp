918414b40d0e8935bdc7715ff482bba5f4af7554
Huy Nguyen <nguyenhuy182515@gmail.com>
2020-08-24 17:21:51 +0700

    create login

--- app/models.py
+++ app/models.py
@@ -1,7 +1,22 @@
-from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy import Column, Integer, String, Float,Boolean, ForeignKey
 from sqlalchemy.orm import relationship
 from flask_admin.contrib.sqla import ModelView
+from flask_admin import BaseView, expose
+from flask import redirect
 from app import db, admin
+from flask_login import UserMixin, current_user, logout_user
+
+
+class User(db.Model, UserMixin):
+    __tablename__ = "user"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(50), nullable=False)
+    active = Column(Boolean, default=True)
+    username = Column(String(50), nullable=False)
+    password = Column(String(50), nullable=False)
+
+    def __str__(self):
+        return self.name
 
 
 class Category(db.Model):
@@ -44,9 +59,48 @@ class Book(db.Model):
         return self.title
 
 
-admin.add_view(ModelView(Category, db.session))
-admin.add_view(ModelView(Author, db.session))
-admin.add_view(ModelView(Book, db.session))
+class CategoryView(ModelView):
+    column_display_pk = True
+    can_create = True
+    form_columns = ("name",)
+
+    def is_accessible(self):
+        return current_user.is_authenticated
+
+
+class AuthorView(ModelView):
+    column_display_pk = True
+    can_create = True
+    form_columns = ("firstname","lastname","dateofbirth",)
+
+    def is_accessible(self):
+        return current_user.is_authenticated
+
+
+class BookView(ModelView):
+    column_display_pk = True
+    can_create = True
+    can_export = True
+
+    def is_accessible(self):
+        return current_user.is_authenticated
+
+
+class LogoutView(BaseView):
+    @expose("/")
+    def index(self):
+        logout_user()
+
+        return redirect("/admin")
+
+    def is_accessible(self):
+        return current_user.is_authenticated
+
+
+admin.add_view(CategoryView(Category, db.session))
+admin.add_view(AuthorView(Author, db.session))
+admin.add_view(BookView(Book, db.session))
+admin.add_view(LogoutView(name="Logout"))
 
 if __name__ == "__main__":
     db.create_all()
